48dfa6485b043e27c2b5c341793193ee8342c60b
Davide Della Casa <davidedc@gmail.com>
2023-07-31 22:22:49 +0100

    can now select a size, click a button and it draws for you many "standard" characters in a nice column

--- index.js
+++ index.js
@@ -1,65 +1,141 @@
-const canvas = document.createElement('canvas');
-const ctx = canvas.getContext('2d');
-var text = '█Sphinx█ g, j, q, p, y';
+// add the size input and run button to the page
+const sizeInput = document.createElement('input');
+sizeInput.id = 'size-input';
+sizeInput.type = 'number';
+sizeInput.value = 10; // on safari on mac, sizes up to 181 are not antialiased, then from 182 onwards are anti-aliased
+document.body.appendChild(sizeInput);
 
-var fontSize = 10;
+const runButton = document.createElement('button');
+runButton.id = 'run-button';
+runButton.textContent = 'Run';
+document.body.appendChild(runButton);
 
-canvas.width = 200;
-canvas.height = 50;
+// append a line break
+document.body.appendChild(document.createElement('br'));
 
-document.body.appendChild(canvas);
 
-ctx.font =  fontSize + 'px Arial';
 
-// size the canvas so it fits the text
-canvas.width = ctx.measureText(text).width;
-canvas.height = fontSize * 1.5;
+runButton.addEventListener('click', () => {
+  const size = parseInt(sizeInput.value);
+  if (!isNaN(size)) {
+    // remove all canvases and divs from the page
+    const canvases = document.querySelectorAll('canvas');
+    for (let i = 0; i < canvases.length; i++) {
+      canvases[i].remove();
+    }
+    const divs = document.querySelectorAll('div');
+    for (let i = 0; i < divs.length; i++) {
+      divs[i].remove();
+    }
 
-// make the background white
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-ctx.fillStyle = 'black';
-//ctx.fillText(text, 10, 30);
+    showCharsAndDataForSize(size);
+  }
+});
 
-// draw the text so that it fits in the canvas
-ctx.textBaseline = 'top';
-ctx.fillText(text, 0, 0);
 
-// get the image data
-const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-const data = imageData.data;
 
-// create a new array with a boolean for each pixel to represent whether it is black or not
-const pixels = [];
-for (let i = 0; i < data.length; i += 4) {
-  const isBlack = data[i] === 0;
-  pixels.push(isBlack);
-}
 
-// do a simple compression of the data by looking for runs of zeros and ones
-const compressedPixels = [];
-let currentPixel = pixels[0];
-let currentPixelCount = 0;
-for (let i = 0; i < pixels.length; i++) {
-  if (currentPixel === pixels[i]) {
-    currentPixelCount++;
-  } else {
-    compressedPixels.push(currentPixelCount);
-    currentPixel = pixels[i];
-    currentPixelCount = 1;
+
+function showCharsAndDataForSize(size) {
+  showCanvasAndCompressedDataForStringAtSize('█', size);
+
+  // run showCanvasAndCompressedDataForStringAtSize for all lower case letters
+  for (let i = 97; i < 123; i++) {
+    const letter = String.fromCharCode(i);
+    showCanvasAndCompressedDataForStringAtSize(letter, size);
+  }
+
+  // run showCanvasAndCompressedDataForStringAtSize for all upper case letters
+  for (let i = 65; i < 91; i++) {
+    const letter = String.fromCharCode(i);
+    showCanvasAndCompressedDataForStringAtSize(letter, size);
+  }
+
+  // run showCanvasAndCompressedDataForStringAtSize for all numbers
+  for (let i = 48; i < 58; i++) {
+    const letter = String.fromCharCode(i);
+    showCanvasAndCompressedDataForStringAtSize(letter, size);
+  }
+
+  allOtherChars = '!"#$%&€\'()*+,-./:;<=>?@[\]^_`{|}~—£°²·ÀÇàç•';
+  // run showCanvasAndCompressedDataForStringAtSize for all chars in allOtherChars
+  for (let i = 0; i < allOtherChars.length; i++) {
+    const letter = allOtherChars[i];
+    showCanvasAndCompressedDataForStringAtSize(letter, size);
   }
 }
-compressedPixels.push(currentPixelCount);
 
-// add a div to the page with the compressed data
-const div = document.createElement('div');
-div.textContent = compressedPixels.join(',');
-document.body.appendChild(div);
+function showCanvasAndCompressedDataForStringAtSize(text, fontSize) {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = 200;
+  canvas.height = 50;
+
+  document.body.appendChild(canvas);
+
+  ctx.font = fontSize + 'px Arial';
+
+  // size the canvas so it fits the text
+  canvas.width = ctx.measureText(text).width;
+  canvas.height = fontSize * 1.5;
+
+  // make the background white
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 
 
+  ctx.fillStyle = 'black';
+  //ctx.fillText(text, 10, 30);
+  // draw the text so that it fits in the canvas
+  ctx.textBaseline = 'top';
+  ctx.font = fontSize + 'px Arial';
+  ctx.fillText(text, 0, 0);
 
+  // get the image data
+  const pixels = getBlackWhitePxsArray(canvas);
+
+  // do a simple compression of the data by looking for runs of zeros and ones
+  const compressedPixels = compressPixels(pixels);
+
+  // add a div to the page with the compressed data
+  const div = document.createElement('div');
+  div.textContent = compressedPixels.join(',');
+  document.body.appendChild(div);
+}
+
+function getBlackWhitePxsArray(canvas) {
+  var ctx = canvas.getContext('2d');
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+
+  // create a new array with a boolean for each pixel to represent whether it is black or not
+  const pixels = [];
+  for (let i = 0; i < data.length; i += 4) {
+    const isBlack = data[i] === 0;
+    pixels.push(isBlack);
+  }
+  return pixels;
+}
+
+function compressPixels(pixels) {
+  const compressedPixels = [];
+  let currentPixel = pixels[0];
+  let currentPixelCount = 0;
+  for (let i = 0; i < pixels.length; i++) {
+    if (currentPixel === pixels[i]) {
+      currentPixelCount++;
+    } else {
+      compressedPixels.push(currentPixelCount);
+      currentPixel = pixels[i];
+      currentPixelCount = 1;
+    }
+  }
+  compressedPixels.push(currentPixelCount);
+  return compressedPixels;
+}
 
 /*
 const canvas = document.createElement('canvas');
