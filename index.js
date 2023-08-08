48f12763d9166f2c2b72c97eb1817933da6f1409
Davide Della Casa <davidedc@gmail.com>
2023-08-08 21:09:25 +0100

    making it so 1) that any change in the form automatically runs everything 2) everything is run at the start c) canvases with test text are added after the form

--- index.js
+++ index.js
@@ -583,7 +583,16 @@ const sizeInput = document.createElement('input');
 sizeInput.id = 'size-input';
 sizeInput.type = 'number';
 sizeInput.value = 80; // on safari on mac, sizes up to 181 are not anti-aliased, then from 182 onwards are anti-aliased
-document.body.appendChild(sizeInput);
+//put inside the "selectors" div
+document.getElementById("selectors").appendChild(sizeInput);
+// run the buildAndShowGlyphs function when the user presses enter in the size input or when the user changes the value
+sizeInput.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    buildAndShowGlyphs();
+  }
+});
+sizeInput.addEventListener('change', buildAndShowGlyphs);
+
 
 // add a dropdown with the font family options
 const fontFamilySelect = document.createElement('select');
@@ -595,7 +604,10 @@ for (let i = 0; i < fontFamilies.length; i++) {
   option.textContent = fontFamilies[i];
   fontFamilySelect.appendChild(option);
 }
-document.body.appendChild(fontFamilySelect);
+document.getElementById("selectors").appendChild(fontFamilySelect);
+// run the buildAndShowGlyphs function when the user changes the value of the font family select
+fontFamilySelect.addEventListener('change', buildAndShowGlyphs);
+
 
 // add a dropdown with the font emphasis options
 const fontEmphasisSelect = document.createElement('select');
@@ -607,20 +619,20 @@ for (let i = 0; i < fontEmphases.length; i++) {
   option.textContent = fontEmphases[i];
   fontEmphasisSelect.appendChild(option);
 }
-document.body.appendChild(fontEmphasisSelect);
-
+document.getElementById("selectors").appendChild(fontEmphasisSelect);
+// run the buildAndShowGlyphs function when the user changes the value of the font emphasis select
+fontEmphasisSelect.addEventListener('change', buildAndShowGlyphs);
 
 
 const runButton = document.createElement('button');
 runButton.id = 'run-button';
 runButton.textContent = 'Build and Show Glyphs';
-document.body.appendChild(runButton);
+document.getElementById("selectors").appendChild(runButton);
 
 // append a line break
 document.body.appendChild(document.createElement('br'));
 
-
-runButton.addEventListener('click', () => {
+function buildAndShowGlyphs() {
   const fontSize = parseInt(sizeInput.value);
   if (!isNaN(fontSize)) {
     // remove all canvases and divs from the page
@@ -635,10 +647,14 @@ runButton.addEventListener('click', () => {
     }
     const divs = document.querySelectorAll('div');
     for (let i = 0; i < divs.length; i++) {
-      divs[i].remove();
+      // remove all divs that don't have the id "selectors"
+      if (divs[i].id !== 'selectors' && divs[i].id !== 'testTextCanvases')
+        divs[i].remove();
     }
   }
-});
+}
+
+runButton.addEventListener('click', buildAndShowGlyphs);
 
 
 function showCharsAndDataForSize(fontSize, fontFamily, fontEmphasis) {
@@ -695,14 +711,88 @@ function showCharsAndDataForSize(fontSize, fontFamily, fontEmphasis) {
   const canvas5 = document.createElement('canvas');
   canvas5.width = 1;
   canvas5.height = 1;
-  document.body.insertBefore(canvas5, document.body.firstChild);
+  document.getElementById("testTextCanvases").appendChild(canvas5);
   const ctx5 = canvas5.getContext('2d');
   ctx5.font = fontEmphasis + " " + fontSize + 'px ' + fontFamily;
   const testTextMeasuresCrisp = ctx5.measureText(testText);
 
 
 
+
+  // add another canvas at the top of the page and draw "Hello World" on it using the CrispBitmapText
+  // add some text above the canvas to say what it is
+  const div = document.createElement('div');
+  div.textContent = 'Crisp Bitmap Text Drawing:';
+  document.getElementById("testTextCanvases").appendChild(div);
+  const canvas = document.createElement('canvas');
+  // TODO need to use own measureText method of the Crisp kind
+  // get the measures of the text from the CrispBitmapText measureText method
+  const crispBitmapText = new CrispBitmapText(crispBitmapGlyphStore);
+  const crispTestTextMeasures = crispBitmapText.measureText(testText, fontSize, fontFamily, fontEmphasis);
+  canvas.width = crispTestTextMeasures.width;
+  canvas.height = crispTestTextMeasures.height * 7;
+  document.getElementById("testTextCanvases").appendChild(canvas);
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  crispBitmapText.drawText(ctx, testText, 0, Math.round(canvas.height - 6 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
+  crispBitmapText.drawText(ctx, testText2, 0, Math.round(canvas.height - 5 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
+  crispBitmapText.drawText(ctx, testText3, 0, Math.round(canvas.height - 4 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
+  crispBitmapText.drawText(ctx, testText4, 0, Math.round(canvas.height - 3 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
+  crispBitmapText.drawText(ctx, testText5, 0, Math.round(canvas.height - 2 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
+  crispBitmapText.drawText(ctx, testText6, 0, Math.round(canvas.height - 1 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
+  crispBitmapText.drawText(ctx, testText7, 0, Math.round(canvas.height - 0 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
+
+
+  // add another canvas at the top of the page and draw "Hello World" on it using the standard canvas text drawing methods
+  // add some text above the canvas to say what it is
+  const div2 = document.createElement('div');
+  div2.textContent = 'Standard Canvas Text Drawing with no smoothing:';
+  document.getElementById("testTextCanvases").appendChild(div2);
+  const canvas2 = document.createElement('canvas');
+  canvas2.width = Math.round(testTextMeasuresCrisp.actualBoundingBoxLeft + testTextMeasuresCrisp.actualBoundingBoxRight);
+  canvas2.height = Math.round(testTextMeasuresCrisp.fontBoundingBoxAscent + testTextMeasures.fontBoundingBoxDescent);
+  // add to DOM before drawing the text otherwise
+  // the CSS property to make it crisp doesn't work
+  document.getElementById("testTextCanvases").appendChild(canvas2);
+  const ctx2 = canvas2.getContext('2d');
+  ctx2.fillStyle = 'white';
+  ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
+  ctx2.fillStyle = 'black';
+  ctx2.font = fontEmphasis + " " + fontSize + 'px ' + fontFamily;
+  ctx2.textBaseline = 'bottom';
+  ctx2.fillText( testText , 0, canvas2.height-1);
+
+
+  
+  // add another canvas at the top of the page and draw "xxxxxxxxxxxx" on it using the standard canvas text drawing methods
+  // add some text above the canvas to say what it is
+  const div6 = document.createElement('div');
+  div6.textContent = 'Standard Canvas Text Drawing with no smoothing - thin characters to see monospaced fonts:';
+  document.getElementById("testTextCanvases").appendChild(div6);
+  const canvas6 = document.createElement('canvas');
+  canvas6.width = Math.round(testTextMeasuresCrisp.actualBoundingBoxLeft + testTextMeasuresCrisp.actualBoundingBoxRight);
+  canvas6.height = Math.round(testTextMeasuresCrisp.fontBoundingBoxAscent + testTextMeasures.fontBoundingBoxDescent);
+  // add to DOM before drawing the text otherwise
+  // the CSS property to make it crisp doesn't work
+  document.getElementById("testTextCanvases").appendChild(canvas6);
+  const ctx6 = canvas6.getContext('2d');
+  ctx6.fillStyle = 'white';
+  ctx6.fillRect(0, 0, canvas6.width, canvas6.height);
+  ctx6.fillStyle = 'black';
+  ctx6.font = fontEmphasis + " " + fontSize + 'px ' + fontFamily;
+  ctx6.textBaseline = 'bottom';
+  ctx6.fillText( '|||||||||||||||||||||||||||||||||||||' , 0, canvas6.height-1);
+
+
+
+
   // add a canvas at the top of the page and draw "Hello World" on it using the standard canvas text drawing methods
+  // add some text above the canvas to say what it is
+  const div3 = document.createElement('div');
+  div3.textContent = 'Standard Canvas Text Drawing with smoothing:';
+  // add inside the testTextCanvases div
+  document.getElementById("testTextCanvases").appendChild(div3);
   const canvas3 = document.createElement('canvas');
   canvas3.width = Math.round(testTextMeasures.actualBoundingBoxLeft + testTextMeasures.actualBoundingBoxRight);
   canvas3.height = Math.round(testTextMeasures.fontBoundingBoxAscent + testTextMeasures.fontBoundingBoxDescent);
@@ -717,73 +807,8 @@ function showCharsAndDataForSize(fontSize, fontFamily, fontEmphasis) {
   ctx3.fillText( testText , 0, canvas3.height-1);
   // add to DOM after drawing the text so
   // the CSS property to make it crisp doesn't work
-  document.body.insertBefore(canvas3, document.body.firstChild);
-  // add some text above the canvas to say what it is
-  const div3 = document.createElement('div');
-  div3.textContent = 'Standard Canvas Text Drawing with smoothing:';
-  document.body.insertBefore(div3, document.body.firstChild);
-  
-  // add another canvas at the top of the page and draw "xxxxxxxxxxxx" on it using the standard canvas text drawing methods
-  const canvas6 = document.createElement('canvas');
-  canvas6.width = Math.round(testTextMeasuresCrisp.actualBoundingBoxLeft + testTextMeasuresCrisp.actualBoundingBoxRight);
-  canvas6.height = Math.round(testTextMeasuresCrisp.fontBoundingBoxAscent + testTextMeasures.fontBoundingBoxDescent);
-  // add to DOM before drawing the text otherwise
-  // the CSS property to make it crisp doesn't work
-  document.body.insertBefore(canvas6, document.body.firstChild);
-  const ctx6 = canvas6.getContext('2d');
-  ctx6.fillStyle = 'white';
-  ctx6.fillRect(0, 0, canvas6.width, canvas6.height);
-  ctx6.fillStyle = 'black';
-  ctx6.font = fontEmphasis + " " + fontSize + 'px ' + fontFamily;
-  ctx6.textBaseline = 'bottom';
-  ctx6.fillText( '|||||||||||||||||||||||||||||||||||||' , 0, canvas6.height-1);
-  // add some text above the canvas to say what it is
-  const div6 = document.createElement('div');
-  div6.textContent = 'Standard Canvas Text Drawing with no smoothing - thin characters to see monospaced fonts:';
-  document.body.insertBefore(div6, document.body.firstChild);
+  document.getElementById("testTextCanvases").appendChild(canvas3);
 
-
-  // add another canvas at the top of the page and draw "Hello World" on it using the standard canvas text drawing methods
-  const canvas2 = document.createElement('canvas');
-  canvas2.width = Math.round(testTextMeasuresCrisp.actualBoundingBoxLeft + testTextMeasuresCrisp.actualBoundingBoxRight);
-  canvas2.height = Math.round(testTextMeasuresCrisp.fontBoundingBoxAscent + testTextMeasures.fontBoundingBoxDescent);
-  // add to DOM before drawing the text otherwise
-  // the CSS property to make it crisp doesn't work
-  document.body.insertBefore(canvas2, document.body.firstChild);
-  const ctx2 = canvas2.getContext('2d');
-  ctx2.fillStyle = 'white';
-  ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
-  ctx2.fillStyle = 'black';
-  ctx2.font = fontEmphasis + " " + fontSize + 'px ' + fontFamily;
-  ctx2.textBaseline = 'bottom';
-  ctx2.fillText( testText , 0, canvas2.height-1);
-  // add some text above the canvas to say what it is
-  const div2 = document.createElement('div');
-  div2.textContent = 'Standard Canvas Text Drawing with no smoothing:';
-  document.body.insertBefore(div2, document.body.firstChild);
-
-
-  // add another canvas at the top of the page and draw "Hello World" on it using the CrispBitmapText
-  const canvas = document.createElement('canvas');
-  // TODO need to use own measureText method of the Crisp kind
-  // get the measures of the text from the CrispBitmapText measureText method
-  const crispBitmapText = new CrispBitmapText(crispBitmapGlyphStore);
-  const crispTestTextMeasures = crispBitmapText.measureText(testText, fontSize, fontFamily, fontEmphasis);
-  canvas.width = crispTestTextMeasures.width;
-  canvas.height = crispTestTextMeasures.height * 7;
-  document.body.insertBefore(canvas, document.body.firstChild);
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'white';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  crispBitmapText.drawText(ctx, testText, 0, Math.round(canvas.height - 6 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
-  crispBitmapText.drawText(ctx, testText2, 0, Math.round(canvas.height - 5 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
-  crispBitmapText.drawText(ctx, testText3, 0, Math.round(canvas.height - 4 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
-  crispBitmapText.drawText(ctx, testText4, 0, Math.round(canvas.height - 3 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
-  crispBitmapText.drawText(ctx, testText5, 0, Math.round(canvas.height - 2 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
-  crispBitmapText.drawText(ctx, testText6, 0, Math.round(canvas.height - 1 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
-  crispBitmapText.drawText(ctx, testText7, 0, Math.round(canvas.height - 0 * crispTestTextMeasures.height -1), fontSize, fontFamily, fontEmphasis);
-  // add some text above the canvas to say what it is
-  const div = document.createElement('div');
-  div.textContent = 'Crisp Bitmap Text Drawing:';
-  document.body.insertBefore(div, document.body.firstChild);
 }
+
+buildAndShowGlyphs();
