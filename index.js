04ff487a7621b0de6e39fdf299be7443efa9779a
Davide Della Casa <davidedc@gmail.com>
2023-09-06 21:13:04 +0100

    moving the specs text to its own file

--- index.js
+++ index.js
@@ -666,55 +666,7 @@ const settingsTextarea = document.createElement('textarea');
 settingsTextarea.id = 'settings-textarea';
 settingsTextarea.style.float = 'left';
 
-settingsTextarea.value = `Arial
-normal
---
-Kerning cutoff
--
-11
---
-Kerning
--
-0 to 20
-  absvds followed by dshkjshdfjhsdfsdfjkh: 0.1
-  sdfslksdf followed by *any*: 0.2
-  *any* followed by LKJLKJF: 0.2
-21 to 22
-  absvds followed by dshkjshdfjhsdfsdfjkh: 0.1
-  sdfslksdf followed by *any*: 0.2
-  *any* followed by LKJLKJF: 0.2
---
-Letters extra space and pull px
--
-0 to 20
-  v:w: right 5 left 9
-  xy: right 6 left 10  
-21 to 22
-  po: right 11 left 12
-23 to 1000
-  ef: right 11 left 12
-  gh: right 13 left 14
---
-Letters extra space and pull proportional
--
-1 to 21
-  hk: right 30 left 0
-22 to 1001
-  lm: right 40 left 0
---
-setting three
-dasdasd
-asdas
----------
-Arial
-bold
---
-setting three
-setting 3 here
---
-setting four
-setting 4 here
-`;
+settingsTextarea.value = specsText;
 document.getElementById("selectors").appendChild(settingsTextarea);
 // settingsTextarea.addEventListener('change', buildAndShowGlyphs);
 settingsTextarea.style.height = '200px';
