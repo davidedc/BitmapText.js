1582e56d9035b07cf1d5b6479aadd8746be6f001
Davide Della Casa <davidedc@gmail.com>
2023-09-10 19:45:47 +0100

    moving all test text to separate file

--- drawTestText.js
+++ drawTestText.js
@@ -1,11 +1,5 @@
-function drawTestText(fontEmphasis, fontSize, fontFamily, crispBitmapGlyphStore) {
-  var testText = 'Access to this information is provided as part of the WorldWideWeb project. The WWW' + "\n" +
-  'project does not take responsability for the accuracy of information provided by others' + "\n" +
-  'References to other information are represented like this. Double-click on it to jump to' + "\n" +
-  'related information.' + "\n" +
-  'Now choose an area in which you would like to start browsing. The system currently has' + "\n" +
-  'access to three sources of information. With the indexes, you should use the keyword' + "\n" +
-  'f to check actualBoundingBoxLeft doesn\'t cause f to be drawn outside the canvas.';
+  function drawTestText(fontEmphasis, fontSize, fontFamily, crispBitmapGlyphStore) {
+  var testText = testText1;
 
   // put the test Text into an array of lines
   const testTextLines = testText.split("\n");
---
+++ test-text.js
@@ -0,0 +1,8 @@
+
+const testText1 = 'Access to this information is provided as part of the WorldWideWeb project. The WWW' + "\n" +
+  'project does not take responsability for the accuracy of information provided by others' + "\n" +
+  'References to other information are represented like this. Double-click on it to jump to' + "\n" +
+  'related information.' + "\n" +
+  'Now choose an area in which you would like to start browsing. The system currently has' + "\n" +
+  'access to three sources of information. With the indexes, you should use the keyword' + "\n" +
+  'f to check actualBoundingBoxLeft doesn\'t cause f to be drawn outside the canvas.';
