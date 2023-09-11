edda2fae5e9b33bdeaff02b9ac24906c9f8b7a84
Davide Della Casa <davidedc@gmail.com>
2023-09-11 17:43:09 +0100

    separate file for font families dropdown ui

--- ui.js
+++ ui.js
@@ -9,18 +9,7 @@ var selectedFontSize = 80;
 
 
 // add a dropdown with the font family options
-const fontFamilySelect = document.createElement('select');
-fontFamilySelect.id = 'font-family-select';
-const fontFamilies = ['Arial', 'Courier New', 'Times New Roman', 'Verdana', 'Georgia', 'Comic Sans MS', 'Impact', 'Lucida Console', 'Lucida Sans Unicode', 'Tahoma', 'Trebuchet MS'];
-for (let i = 0; i < fontFamilies.length; i++) {
-  const option = document.createElement('option');
-  option.value = fontFamilies[i];
-  option.textContent = fontFamilies[i];
-  fontFamilySelect.appendChild(option);
-}
-document.getElementById("selectors").appendChild(fontFamilySelect);
-// run the buildAndShowGlyphs function when the user changes the value of the font family select
-fontFamilySelect.addEventListener('change', buildAndShowGlyphs);
+const fontFamilySelect = addDropdownWithFontFamilies();
 
 
 // add a dropdown with the font emphasis options
