ac1a0e12ba27424817e79c0efafcc05774133c2b
Davide Della Casa <davidedc@gmail.com>
2023-10-03 11:36:44 +0100

    adjusting kerning of size 14

--- specs.js
+++ specs.js
@@ -8,6 +8,18 @@ Kerning cutoff
 --
 Kerning
 -
+14 to 14
+  U followed by ade: 50
+  YT followed by dklacegijmnopqrsuvwxyz.,:;—·Çàç•: 50
+  V followed by dklacegijmnopqrsuvwxyz.,:;—·Çàç•: 50
+  IJ followed by *any*: 50
+13 to 13
+  W followed by i: -50
+  *any* followed by ': 100
+  ' followed by *any*: -50
+  YT followed by dklacegijmnopqrsuvwxyz.,:;—·Çàç•: 150
+  V followed by dklacegijmnopqrsuvwxyz.,:;—·Çàç•: 50
+  I followed by *any*: 50
 11 to 11
   k followed by wl: 50
   f followed by yilhb: -50
@@ -116,21 +128,21 @@ Kerning
   o followed by *any*: -0.2
   w followed by *any*: -0.2
   k followed by ae: 0.1
-13 to 21
+15 to 21
   // letter === 'A' && this.isShortCharacter(nextLetter)
   A followed by acegijmnopqrsuvwxyz.,:;—·Çàç•: 0.1
   *any* followed by aetof: 0.1
   f followed by o: 0.2
   fteo followed by *any*: 0.1
   *any* followed by j: 0.1
-13 to 20
+15 to 20
   ftvy followed by ftvy: 0.1
   rk followed by *any*: 0.1
   p followed by a: 0.1
   c followed by y: 0.1
 21 to 23
   *any* followed by j: -0.15
-13 to 100
+15 to 100
   WV followed by WV: -0.04
   A\\L followed by W7: 0.1
   WfP followed by A/: 0.1
@@ -174,7 +186,10 @@ Kerning discretisation for small sizes
 // Happens at small sizes due to a browser rendering
 CropLeft correction px
 -
-10 to 10 at pixel density 1
+14 to 14 at pixel density 1
+  .: -1
+13 to 14 at pixel density 1
+  ,: -110 to 10 at pixel density 1
   Wsk: 1
 11 to 11
   // W is clipped on the left, this correction will simply paint them
